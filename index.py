7ea54b19768e77f3565443ffa85b78ef1800ca16
Denis Ndiritu <denisndiritu1@gmail.com>
2025-04-15 16:06:24 +0300

    Added the Error Handling task

--- index.py
+++ index.py
@@ -1,3 +1,5 @@
+# File Read & Write Challenge 🖋️: Create a program that reads a file and writes a modified version to a new file.
+
 input_file = "input.txt"
 output_file = "output.txt"
 
@@ -8,3 +10,18 @@ with open(input_file, "r") as infile, open(output_file, "w") as outfile:
     # Add extra lines after writing the original content
     outfile.write("\nThis is a new line added to the output_file\n")
     outfile.write("Another added line.")
+
+# Error Handling Lab 🧪: Ask the user for a filename and handle errors if it doesn’t exist or can’t be read.
+filename  = input("Enter the filename you want to read: ")
+try:
+    with open(filename, "r") as file:
+        content = file.read()
+        print(content)
+except FileNotFoundError:
+    print(f"The file '{filename}' was not found.")
+
+except PermissionError:
+    print(f"You do not have permission to read the file '{filename}'.")
+
+except Exception as e:
+    print(f"An error occurred: {e}")
